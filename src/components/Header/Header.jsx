5257448b842462f091d61860e3d8b34c323e6940
Kirill Kozlov <kirillkozlove39@gmail.com>
2022-05-16 17:59:36 +0300

    add main wrapper and Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Header.module.css"
+
+const Header = ({text}) => {
+
+   return (
+      <header className={styles.Header}>
+         <h1 className={styles.name}>{text}</h1>
+      </header>
+   );
+};
+
+export default Header;

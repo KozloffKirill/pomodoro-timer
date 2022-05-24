f104600d7f831ca863a53df8762503e7026c0859
Kirill Kozlov <kirillkozlove39@gmail.com>
2022-05-24 14:45:18 +0300

    created and implemented "TasksList" component

---
+++ src/components/TasksList/TasksList.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { TasksContext } from "../../contexts/TasksContext";
+import styles from "./TasksList.module.css";
+import Task from "../Task/Task";
+
+const TasksList = () => {
+   const { tasks } = useContext(TasksContext);
+
+   return (
+      <ul className={styles.taskList}>
+         {
+            tasks?.map((task) => {
+               return (
+                  <li key={task.id}>
+                     <Task data={task} />
+                  </li>
+               );
+            })
+         }
+      </ul>
+   );
+}
+
+export default TasksList;
